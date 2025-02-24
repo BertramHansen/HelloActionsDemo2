f36b618a295cd0dd2b5406e076547943da00e4e5
BertramHansen <beha0007@stud.kea.dk>
2025-02-24 14:11:09 +0100

    Initial commit

---
+++ src/test/java/org/example/helloactionsdemo/HelloActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.helloactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
