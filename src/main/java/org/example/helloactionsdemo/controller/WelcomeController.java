80fa73a47fc86b0f8b698ce615fde83bb8bda52c
BertramHansen <beha0007@stud.kea.dk>
2025-02-26 08:47:12 +0100

    controller and html made

--- src/main/java/org/example/helloactionsdemo/controller/WelcomeController.java
+++ src/main/java/org/example/helloactionsdemo/controller/WelcomeController.java
@@ -2,15 +2,16 @@ package org.example.helloactionsdemo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class WelcomeController {
 
     public WelcomeController(){
     }
 
 
-    @GetMapping()
+    @GetMapping("/welcome")
     public String welcomeString() {
         return "welcome";
     }
